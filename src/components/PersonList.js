2d1e4a55133b8979bd203858e9c012b36645ffe6
travisly <travis.ly@hotmail.com>
2023-02-16 20:49:28 +0000

    Update PersonList.js
    Use https://66kogabum0.execute-api.us-west-2.amazonaws.com/Prod/get-todo API end point

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -7,7 +7,7 @@ export default class PersonList extends React.Component {
   }
 
   componentDidMount() {
-    axios.get(`https://q76xnqzf38.execute-api.us-west-2.amazonaws.com/prod/get-person`)
+    axios.get(`https://66kogabum0.execute-api.us-west-2.amazonaws.com/Prod/get-todo`)
       .then(res => {
         const persons = res.data.body;
         this.setState({ persons });
